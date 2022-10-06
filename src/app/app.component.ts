ad85beaf4cfa24ead5b3af2ac2205ba084197527
juan <jucagal@hotmail.com>
2022-10-06 08:36:20 -0500

    add pdf view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {PDFDocument} from 'pdf-lib'
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,10 @@ export class AppComponent {
   allFileList: File[] = [];
   groupsFileList: File[][] = [];
   blocksNames: (CdkDropList | string)[] = ['cdk-drop-list-0'];
+  pdfBytes: Uint8Array = new Uint8Array;
+  showModal: boolean = false;
+  displayModal!: boolean;
+  headerFile!: string;
 
   cleanGroups() {
     this.allFileList = [];
@@ -49,5 +54,36 @@ export class AppComponent {
         event.currentIndex,
       );
     }
+  }
+
+  async showPdf(file: File) {
+    const pdfDoc = await PDFDocument.create()
+    const firstDonorPdfBytes = await file.arrayBuffer();
+    const secondDonorPdfBytes = await file.arrayBuffer();
+    const firstDonorPdfDoc = PDFDocument.load(firstDonorPdfBytes)
+    const secondDonorPdfDoc = PDFDocument.load(secondDonorPdfBytes)
+    const [firstDonorPage] = await pdfDoc.copyPages(await firstDonorPdfDoc, [0])
+    const [secondDonorPage] = await pdfDoc.copyPages(await secondDonorPdfDoc, [0])
+    pdfDoc.addPage(firstDonorPage)
+    pdfDoc.insertPage(0, secondDonorPage)
+    pdfDoc.insertPage(0, secondDonorPage)
+    pdfDoc.insertPage(0, secondDonorPage)
+    pdfDoc.insertPage(0, secondDonorPage)
+    this.pdfBytes = await pdfDoc.save();
+  }
+
+  callModal(file: File) {
+    this.pdfBytes = new Uint8Array;
+    this.showPdf(file);
+    this.showModal = true;
+    this.headerFile = file.name;
+    /*file.arrayBuffer().then(buff => {
+      this.pdfBytes = new Uint8Array(buff);
+    }).finally(() => {
+      this.showModal = true;
+    }).catch(e => {
+      this.showModal = false;
+      console.error(e);
+    });*/
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {DragDropModule} from "@angular/cdk/drag-drop";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {PdfViewerModule} from 'ng2-pdf-viewer';
+import {DialogModule} from 'primeng/dialog';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ButtonModule} from "primeng/button";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        DragDropModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DragDropModule,
+    DialogModule,
+    PdfViewerModule,
+    BrowserAnimationsModule, ButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
